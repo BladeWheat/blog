31ada655db0b9d775b0a2b15c8aa133a6646628f
BladeWheat <2903991699@qq.com>
2024-03-29 15:35:33 +0800

    Site updated: 2024-03-29 15:35:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/20/hello-world/","2024/03/23/我的第一篇博客/","2024/03/26/欢迎来到我的博客！！！/"];function toRandomPost(){
+var posts=["2024/03/23/我的第一篇博客/","2024/03/20/hello-world/","2024/03/26/欢迎来到我的博客！！！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
