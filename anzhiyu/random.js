cab64cc2f3e2a344742df77055181fd84a253732
BladeWheat <2903991699@qq.com>
2024-03-31 23:50:08 +0800

    Site updated: 2024-03-31 23:49:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/31/3DS汉化游戏（正在收录中……）/","2024/03/26/欢迎来到我的博客！！！/"];function toRandomPost(){
+var posts=["2024/03/26/欢迎来到我的博客！！！/","2024/03/31/3DS汉化游戏（正在收录中……）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
