0beae12b93b0e8d94d4a2214f555613dbbbffc6a
BladeWheat <2903991699@qq.com>
2024-04-02 17:25:24 +0800

    Site updated: 2024-04-02 17:25:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/31/3DS汉化游戏（正在收录中……）/","2024/03/26/欢迎来到我的博客！！！/","2024/04/02/精灵宝可梦XY(汉化)/"];function toRandomPost(){
+var posts=["2024/03/26/欢迎来到我的博客！！！/","2024/04/02/精灵宝可梦XY(汉化)/","2024/03/31/3DS汉化游戏（正在收录中……）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
